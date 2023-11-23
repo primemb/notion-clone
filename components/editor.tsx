7ef889d59bad8e4db157d37a8cb51cc416b23ae0
Mohammad Reza <mo.behzad34@gmail.com>
2023-11-23 14:03:40 +0330

    finished editor stuff

---
+++ components/editor.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { BlockNoteEditor, PartialBlock } from "@blocknote/core";
+import { BlockNoteView, useBlockNote } from "@blocknote/react";
+
+import "@blocknote/core/style.css";
+import { useTheme } from "next-themes";
+import { useEdgeStore } from "@/lib/edgestore";
+
+interface EditorProps {
+  initialContent?: string;
+  editable?: boolean;
+  onChange: (content: string) => void;
+}
+
+export const Editor = ({
+  initialContent,
+  editable = true,
+  onChange,
+}: EditorProps) => {
+  const { resolvedTheme } = useTheme();
+  const { edgestore } = useEdgeStore();
+
+  const handleUpload = async (file: File): Promise<string> => {
+    const response = await edgestore.publicFiles.upload({ file });
+    return response.url;
+  };
+
+  const editor: BlockNoteEditor = useBlockNote({
+    editable,
+    initialContent: initialContent ? JSON.parse(initialContent) : undefined,
+    onEditorContentChange: (editor) => {
+      onChange(JSON.stringify(editor.topLevelBlocks, null, 2));
+    },
+    uploadFile: handleUpload,
+  });
+
+  return (
+    <div>
+      <BlockNoteView
+        editor={editor}
+        theme={resolvedTheme === "dark" ? "dark" : "light"}
+      />
+    </div>
+  );
+};
